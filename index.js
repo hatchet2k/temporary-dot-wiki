1a81788b7fd202bfe7ef3d036345d3386fa7c715
hatchet2k <124828533+hatchet2k@users.noreply.github.com>
2023-03-31 14:25:38 -0700

    sizing test with black bg

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ warp.transform(([ x, y ]) => [ x, y, y ])
 let offset = 0
 function animate()
 {
-    warp.transform(([ x, y, oy ]) => [ x, oy + 4 * Math.sin(x / 16 + offset), oy ])
+    warp.transform(([ x, y, oy ]) => [ x, oy + 16 * Math.sin(x / 32 + offset), oy ])
     offset += 0.1
     requestAnimationFrame(animate)
 }
